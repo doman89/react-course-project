9432cee7ef1d413f3d38939b0ec8647f86096275
Mateusz Domanski <mdoma@softserveinc.com>
2020-09-11 18:47:04 +0200

    feat: User login/logout in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Header from './components/Header';
+import StoreProvider from './store/StoreProvider';
+
 import './App.scss';
 
 const App = () => (
-	<header>Hello World!</header>
+	<StoreProvider>
+		<Header />
+	</StoreProvider>
 );
 
 export default App;
